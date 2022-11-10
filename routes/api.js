42d9852045297b4e1a7ed79da30fafa7206602cf
Aulia Rahman <114418374+liaacans@users.noreply.github.com>
2022-11-10 19:18:58 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -8,10 +8,10 @@ var zahirr = db.get("zahirr");
 	console.log('')  
 }
  
-var creator = "Rey"
+var creator = "Aulia Rahman"
 var neoxr = "yntkts"
-var zeks = "administrator"
-var zeks2 = "apivinz"
+var zeks = "freeapi"
+var zeks2 = "auliahostApi"
 var secure = require('ssl-express-www');
 var cors = require('cors');
 var fetch = require('node-fetch');
